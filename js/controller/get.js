9d4ad5856acef95441020bf89812cd353d7b220e
daniferdinandall <daniferdinandall@gmail.com>
2023-03-31 19:42:06 +0700

    6-2B-1214050-DANI FERDINAN

--- js/controller/get.js
+++ js/controller/get.js
@@ -12,7 +12,7 @@ function isiRow(value) {
             .replace("#JABATAN#", value.biodata.jabatan)
             .replace("#LOKASI#", value.location)
             .replace("#STATUS#", value.checkin)
-            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja?value.biodata.hari_kerja:"HARIKERJA")
             .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
             .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
             .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
