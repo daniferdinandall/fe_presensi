bed55b862d140b6bc521f5bc72473dcee8cb5be5
daniferdinandall <daniferdinandall@gmail.com>
2023-03-31 14:55:55 +0700

    add tabble

--- js/controller/fetch.js
+++ js/controller/fetch.js
@@ -1,11 +1,24 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-
+import { isiTabel } from "./table.js";
 let urlAPI = "https://ws-dani.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+// function isiRow(value){
+//     console.log(value)
+// }
+
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
