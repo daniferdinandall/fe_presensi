f669b1ba7c9c5de647675052187621121957547d
daniferdinandall <daniferdinandall@gmail.com>
2023-03-31 14:35:47 +0700

    update

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,16 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
+// function get(target_url,responseFunction){
+//     var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//     };
+
+//     fetch(target_url, requestOptions)
+//     .then(response => response.text())
+//     .then(result => responseFunction(JSON.parse(result)))
+//     .catch(error => console.log('error', error));
+// }
+
 let urlAPI = "https://ws-dani.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
